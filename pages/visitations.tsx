ca25c6d786e98b61919d23d6ea6dee824630d877
phmuniz <phmcarvalho@hotmail.com.br>
2024-11-06 20:47:13 -0300

    update

--- pages/visitations.tsx
+++ pages/visitations.tsx
@@ -20,12 +20,20 @@ const Visitations: NextPage = () => {
 
                 <h4>As visitas guiadas serão divididas em Trilhas Temáticas, da seguinte forma:</h4>
                 <ul>
-                <li><span>Trilha 1:</span> Engenharia Ambiental {/*- NQUALIAR, LAGESA, LABSAN, LABHIDRO*/}</li>
-                    <li><span>Trilha 2:</span> Engenharia Elétrica 01 {/*- LABTEL, NERDS, NTA e Laboratório de Intrumentação Dinâmica*/}</li>
-                    <li><span>Trilha 3:</span> Engenharia Elétrica 02 {/*- LABVISIO, PET ELÉTRICA, LAI, LCI, LEPAC/LACI*/}</li>
-                    <li><span>Trilha 4:</span> Engenharia Civil {/*- GEOTECNIA, LABDEST, LEMAC, L@BCIV, LABESBIM*/}</li>
-                    <li><span>Trilha 5:</span> Computação {/*- PET ENGCOMP, ERUS, LCAD, LPRM, NINFA, LAR*/}</li>
-                    <li><span>Trilha 6:</span> Engenharia Mecânica {/*- NEMOG, PET MECÂNICA, LaMEFT/DEM, Laboratório de automação e controle*/}</li>
+                    <li><span>Trilha 1 (manhã):</span> Engenharia Elétrica 01 - LECO, PET Elétrica e LEPAC</li>
+                    <li><span>Trilha 1 (tarde):</span> Engenharia Elétrica 01 - LABVISIO, PET Elétrica e LEPAC</li>
+                    <li><span>Trilha 2 (manhã):</span> Engenharia Elétrica 02 - LRTA, LABVISIO e LABTEL</li>
+                    <li><span>Trilha 2 (tarde):</span> Engenharia Elétrica 02 - LABAIR e LRTA</li>
+                    <li><span>Trilha 3:</span> Engenharia Mecânica 01 - LaMEFT, LabSensores e LabGuará</li>
+                    <li><span>Trilha 4:</span> Engenharia Mecânica 02 - LaTecMec e TriCorrMat</li>
+                    <li><span>Trilha 5:</span> Engenharia Civil - Geotecnia, LabDest, Lemac, L@bCIV e LabesBIM</li>
+                    <li><span>Trilha 6:</span> Engenharia Ambiental - LabHidro, LabSan e NqualirAr</li>
+                    <li><span>Trilha 7:</span> Computação 01 - LCAD, PET Eng Comp e ERUS</li>
+                    <li><span>Trilha 8:</span> Computação 02 - Informática na Saúde (postos no auditório do CT 13)</li>
+                    <li><span>Trilha 9 (manhã):</span> Engenharia de Produção - Laboratório de Manufatura Integrada, LIEP, Modelar e Sala de Metodologias Ativas</li>
+                    <li><span>Trilha 9 (tarde):</span> Engenharia de Produção - LABTar, Laboratório de Manufatura Integrada, LIEP e Modelar</li>
+                    <li><span>Trilha 10:</span> Inovação - Espaço Empreendedor (antigo prédio da gráfica universitária)</li>
+                    
                 </ul>
                             
                     
